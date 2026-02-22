8bb7796828c1df7250494367f7dd2e45cbb5f7fd
page <iykyk0325@gmail.com>
2026-02-22 16:22:33 +0900

    완강 후 코드 업로드

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.*;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+//@Repository
+public class MemoryMemberRepository implements MemberRepository {
+
+    private Map<Long, Member> store;
+    private long sequence;
+
+    // 생성자 함수를 통해, 인스턴스 변수 초기화
+    public MemoryMemberRepository() {
+        this.store = new HashMap<>();
+        this.sequence = 0L;
+    }
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
